eab0d9735409c8dfacdeff205bac5289cff5b768
Marcel Pinto <p.marcel.augusto@gmail.com>
2020-10-07 18:02:10 -0300

    Validacao dos formularios

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -4,12 +4,16 @@ import {
   KeyboardAvoidingView,
   ScrollView,
   TextInput,
+  Alert,
 } from 'react-native';
+import * as Yup from 'yup';
+
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
 
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -24,17 +28,47 @@ import {
   CreateAccountButtonText,
 } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const passwordInputRef = useRef<TextInput>(null);
 
   const navigation = useNavigation();
-  const handleSubmit = useCallback(
-    (data: { name: string; password: string }) => {
-      console.log(data);
-    },
-    [],
-  );
+  const handleSignIn = useCallback(async (data: SignInFormData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Digite seu email')
+          .email('Digite um e-mail válido'),
+        password: Yup.string().required('Digite sua senha'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+
+      // const { email, password } = data;
+
+      // await signIn({ email, password });
+
+      // history.push('/dashboard');
+    } catch (error) {
+      if (error instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(error);
+        formRef.current?.setErrors(errors);
+        return;
+      }
+
+      Alert.alert(
+        'Erro na autenticação',
+        'Ocorreu um erro ao fazer login, cheque suas credenciais.',
+      );
+    }
+  }, []);
 
   return (
     <>
@@ -50,7 +84,7 @@ const SignIn: React.FC = () => {
 
             <Title>Faça seu logon</Title>
 
-            <Form ref={formRef} onSubmit={handleSubmit}>
+            <Form ref={formRef} onSubmit={handleSignIn}>
               <Input
                 autoCapitalize="none"
                 autoCorrect={false}
